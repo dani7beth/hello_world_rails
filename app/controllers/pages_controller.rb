bbc8570fe7cf80e01f2baecd3fde7db360b02256
dani7beth <danielledona28@gmail.com>
2020-11-16 14:44:26 -0700

    added some more pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,5 +13,47 @@ class PagesController < ApplicationController
   end
 
   def new
+    #.new just makes a new page object in memory 
+    #.create store to database
+    @page = Page.new
+  end
+  def create
+    @page = Page.new(pages_params)
+
+    # this is always go work for now
+    if @page.save
+      redirect_to pages_path
+    else
+      # might have validations like must be present
+      # we return user to the new form
+      render :new
+    end
+  end
+  
+  def edit
+    #return form here
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    page = Page.find(params[:id])
+    page.destroy
+    redirect_to pages_path
+  end
+
+  private
+
+  def pages_params
+    params.require(:page).permit(:title, :author, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
+  #root route
+  root 'static_pages#home'
+  # get 'static_pages/home'
+  # get 'static_pages/about'
+  # get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
 
+  #custom route
+  get '/about', to: 'static_pages#about'
+
+  #generates crud action routes in this case for pages
   resources :pages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
