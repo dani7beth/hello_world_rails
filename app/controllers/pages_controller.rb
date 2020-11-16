e2a0dc26b137c2bf7c6fb4f320a2ddfc8e686fe8
dani7beth <danielledona28@gmail.com>
2020-11-16 11:56:58 -0700

    updated index,show, and pagescontroller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
+  #just grab all pages (generally for crud but not all the time)
   def index
+    @pages = Page.all
+    #rails convention... if you dont explicitly tell rails what to return here
+    #it will automatically look in views/pages/index.html.erb and return that
   end
 
+  #just grab one page
   def show
+    @page = Page.find(params[:id])
+    #it will automatically look in views/pages/show.html.erb 
   end
 
   def new
